c69504900b525fe020c6929b8e450073710677a9
jinling <3282816173@qq.com>
2024-12-03 22:01:59 +0800

    把抽奖做完了

--- 学校作业/抽奖/index.js
+++ 学校作业/抽奖/index.js
@@ -5,3 +5,7 @@ function 抽奖() {
   const 用户抽奖结果 = 奖品[后台抽奖];
   document.getElementById('结果').innerText=`恭喜你获得：${用户抽奖结果}`;
 }
+
+document.getElementById('抽奖按钮').addEventListener('click', function() {
+  document.getElementById('抽奖结果').classList.toggle('flipped'); // 点击时切换 flipped 类
+});
